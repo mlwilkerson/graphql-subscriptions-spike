b4b69325bcc8df024debe3dac552ef0ec6d4771e
Christopher Bartling <chris.bartling@gmail.com>
2017-12-26 12:14:06 -0600

    Up and running.

--- graphql-subscriptions-apollo-express/app.js
+++ graphql-subscriptions-apollo-express/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const {graphiqlExpress, graphqlExpress} = require('apollo-server-express');
 const schema = require('./graphql/schema');
-const database = require('./database/database');
+// const database = require('./database/database');
 const helmet = require('helmet');
 
 
@@ -11,12 +11,20 @@ const helmet = require('helmet');
 const PORT = 4000;
 
 const app = express();
+const helperMiddleware = [
+    bodyParser.json(),
+    bodyParser.text({ type: 'application/graphql' }),
+    (req, res, next) => {
+        if (req.is('application/graphql')) {
+            req.body = { query: req.body };
+        }
+        next();
+    }
+];
 
-app.use(cors);
+app.use(cors());
 app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+app.use('/graphql', ...helperMiddleware, graphqlExpress({schema}));
 app.get('/graphiql', graphiqlExpress({endpointURL: '/graphql'})); // if you want GraphiQL enabled
 app.listen(PORT, () => {
     console.log(`GraphQL API is available at http://localhost:${PORT}/graphql`);
--- graphql-subscriptions-apollo-express/graphql/schema.js
+++ graphql-subscriptions-apollo-express/graphql/schema.js
@@ -5,45 +5,25 @@ const {Post} = require('../database/models');
 
 
 const typeDefs = `
-    type Comment { 
-        id: Int!
-        body: String! 
-    }
-
-    type Post { 
-        id: Int!
-        title: String! 
-        body: String! 
-        comments: [Comment]
-    }
-
     type Query { 
-        posts: [Post] 
-    }
-  
-    type Mutation {
-        createPost (
-            title: String!, 
-            body: String!): Post
+        helloWorld: String
     }
 `;
 
 const resolvers = {
     Query: {
-        posts: () => {
-            return Post.findAll({})
+        helloWorld: () => {
+            return "Hello world! ;-)";
         }
     },
-    Mutation: {
-        createPost: (_, {title, body}) => {
-            const post = Post.create({title: title, body: body});
-            // pubsub.publish('postsChanged', {postsChanged: {id: post.id, change: 'CREATE'}});
-            return post;
-        },
-    }
+    // Mutation: {
+    //     createPost: (_, {title, body}) => {
+    //         const post = Post.create({title: title, body: body});
+    //         // pubsub.publish('postsChanged', {postsChanged: {id: post.id, change: 'CREATE'}});
+    //         return post;
+    //     },
+    // }
 };
 
 
-const schema = makeExecutableSchema({typeDefs, resolvers});
-
-module.exports = schema;
+module.exports = makeExecutableSchema({typeDefs, resolvers});
