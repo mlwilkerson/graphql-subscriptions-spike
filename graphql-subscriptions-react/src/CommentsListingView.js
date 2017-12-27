256b9ce11221c820252426102749f25ce7bf182a
Christopher Bartling <chris.bartling@gmail.com>
2017-12-27 10:50:53 -0600

    Adding GraphQL query to comments listing view.

--- graphql-subscriptions-react/src/CommentsListingView.js
+++ graphql-subscriptions-react/src/CommentsListingView.js
@@ -1,24 +1,46 @@
 import React, {Component} from "react";
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+
+const commentsQuery = gql`
+    query CommentsForPost($postId: ID!) { 
+        comments (postId: $postId) { 
+            id
+            body
+        } 
+    }
+`;
+
+const withCommentsData = graphql(commentsQuery, {
+    options: ({postId}) => ({variables: {postId}})
+});
+
 
 class CommentsListingView extends Component {
 
     render() {
         let content = null;
-        if (this.props.comments) {
-            content = this.props.comments.map((comment) => {
-                return (<li key={comment.id}>{comment.body}</li>)
-            });
+        if (this.props.data) {
+            if (this.props.data.loading) {
+                content = (<div>Data loading! Please wait...</div>);
+            } else if (this.props.data.error) {
+                content = (<div className="text-danger">An error occurred. {this.props.data.error}</div>);
+            } else if (this.props.data.comments) {
+                content = (
+                    <ul>
+                        {this.props.data.comments.map((comment) => (<li key={comment.id}>{comment.body}</li>))}
+                    </ul>
+                )
+            }
         }
+
         return (
             <div>
                 <h4>Comments</h4>
-                <ul>
-                    {content}
-                </ul>
+                {content}
             </div>
         );
     }
 }
 
-// export default withPostsData(PostsListingView);
-export default CommentsListingView;
+export default withCommentsData(CommentsListingView);
--- graphql-subscriptions-react/src/PostsListingView.js
+++ graphql-subscriptions-react/src/PostsListingView.js
@@ -3,7 +3,7 @@ import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 import CommentsListingView from "./CommentsListingView";
 
-const postsSubscription = gql`subscription postAdded { id title body }`;
+// const postsSubscription = gql`subscription postAdded { id title body }`;
 
 const postsQuery = gql`
 { 
@@ -26,7 +26,7 @@ class PostsListingView extends Component {
 
     render() {
         let content = (<div>&nbsp;</div>);
-        if (this.props.data){
+        if (this.props.data) {
             if (this.props.data.loading) {
                 content = (<div>Data loading! Please wait...</div>);
             } else if (this.props.data.error) {
@@ -36,7 +36,7 @@ class PostsListingView extends Component {
                     <div className="" key={post.id}>
                         <h3>{post.title}</h3>
                         <div className="">{post.body}</div>
-                        <CommentsListingView comments={post.comments}/>
+                        <CommentsListingView postId={post.id}/>
                     </div>
                 );
                 content = this.props.data.posts.map(func)
