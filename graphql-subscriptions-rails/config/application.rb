854e7169be6ba06b13b9bbc1526d0970c0e551bf
Christopher Bartling <chris.bartling@gmail.com>
2018-01-21 21:40:51 -0600

    Adding resque to the mix.

--- graphql-subscriptions-rails/config/application.rb
+++ graphql-subscriptions-rails/config/application.rb
@@ -15,6 +15,7 @@ module GraphqlSubscriptionsRails
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.active_job.queue_adapter = :resque
 
     # see https://github.com/cyu/rack-cors
     config.middleware.insert_before 0, Rack::Cors do
