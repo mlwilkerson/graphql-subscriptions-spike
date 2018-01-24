344d953233e480674886f6585730c544380c39c7
Christopher Bartling <chris.bartling@gmail.com>
2018-01-24 16:21:15 -0600

    Nginx configuration for CORS is now working.

--- graphql-subscriptions-rails/config/initializers/resque.rb
+++ graphql-subscriptions-rails/config/initializers/resque.rb
@@ -6,5 +6,5 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 Resque.redis = resque_config[rails_env]
 
-Resque.logger = Logger.new(File.open("#{Rails.root}/log/#{rails_env}_resque.log", 'w+'))
+Resque.logger = Logger.new(File.open("#{Rails.root}/resque-log/#{rails_env}_resque.log", 'w+'))
 Resque.logger.formatter = Resque::VeryVerboseFormatter.new
